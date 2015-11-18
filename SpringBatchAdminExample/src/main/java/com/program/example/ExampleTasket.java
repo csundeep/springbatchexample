7b6403b37d8c28b28a952ebda2421a6b86c32f81
schilukuri <schilukuri@gogoair.com>
2015-11-18 19:38:55 +0530

    Added spring batch admin project

---
+++ SpringBatchAdminExample/src/main/java/com/program/example/ExampleTasket.java
@@ -0,0 +1,20 @@
+package com.program.example;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExampleTasket implements Tasklet {
+	public ExampleTasket() {
+		System.out.println("########################");
+	}
+	
+	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
+		System.out.println("Calling tasklet");
+		return RepeatStatus.FINISHED;
+	}
+	
+}
---
+++ SpringBatchAdminExample/src/main/java/com/program/example/TestScheduler.java
@@ -0,0 +1,29 @@
+package com.program.example;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class TestScheduler implements Runnable {
+	@Autowired
+	private JobLauncher jobLauncher;
+	@Autowired
+	private Job runJob;
+	
+	@Scheduled(cron = "* 0/1 * * * ?")
+	public void run() {
+		try {
+			System.out.println("starting JOB.................");
+			JobParameters param = new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters();
+			jobLauncher.run(runJob, param);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
